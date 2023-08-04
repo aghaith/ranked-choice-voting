734e6390114a37126263e85b97d96f3bda7014b0
abbasghaith23 <abbasghaith23@gmail.com>
2023-08-04 16:03:10 +0400

    Ranked choice voting app

---
+++ server/src/polls/polls.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, /* Logger, */ Post } from '@nestjs/common';
+import { CreatePollDto, JoinPollDto } from './dtos';
+import { PollsService } from './polls.service';
+
+@Controller('polls')
+export class PollsController {
+  constructor(private pollsService: PollsService) {}
+
+  @Post()
+  async create(@Body() createPollDto: CreatePollDto) {
+    const result = await this.pollsService.createPoll(createPollDto);
+    // Logger.log('In create!');
+    // return createPollDto;
+    return result;
+  }
+
+  @Post('/join')
+  async join(@Body() joinPollDto: JoinPollDto) {
+    const result = await this.pollsService.joinPoll(joinPollDto);
+    // Logger.log('In join!');
+    // return joinPollDto;
+    return result;
+  }
+
+  @Post('/rejoin')
+  async rejoin() {
+    const result = await this.pollsService.rejoinPoll({
+      name: 'From token',
+      pollID: 'Also from token',
+      userID: 'Guess where this comes from?',
+    });
+    // Logger.log('In rejoin!');
+    return result;
+  }
+}
